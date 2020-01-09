542ab353995243abe7f2bd3fd89a7f7483b84f1c
Saghi Shiri <saghishiri@gmail.com>
2020-01-09 20:38:16 +0330

    Add fake api
    Set Proxy to avoid CORS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { WizardComponent } from './wizard/wizard.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { ProjectSettingsComponent } from './project-settings/project-settings.component';
+import { DeliveryDetailsComponent } from './delivery-details/delivery-details.component';
+import { ReviewAndSubmitComponent } from './review-and-submit/review-and-submit.component';
+import { ProjectsListComponent } from './projects-list/projects-list.component';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './store/reducer';
+import { StepperComponent } from './stepper/stepper.component';
+
+const appRoutes: Routes = [
+  { path: 'wizard', component: WizardComponent, children: [
+      { path: 'project-details', component: ProjectDetailsComponent },
+      { path: 'project-settings', component: ProjectSettingsComponent },
+      { path: 'delivery-details', component: DeliveryDetailsComponent },
+      { path: 'review', component: ReviewAndSubmitComponent },
+    ] },
+  { path: '', redirectTo: 'wizard/project-details', pathMatch: 'full' },
+  { path: 'projects', component: ProjectsListComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WizardComponent,
+    ProjectDetailsComponent,
+    ProjectSettingsComponent,
+    DeliveryDetailsComponent,
+    ReviewAndSubmitComponent,
+    ProjectsListComponent,
+    StepperComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false }
+    ),
+    HttpClientModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot({project: reducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
