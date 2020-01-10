6c1fa1518f9dbd60c34ad753a033f36015a50fc3
Saghi Shiri <saghishiri@gmail.com>
2020-01-10 11:59:45 +0330

    Change variable names

--- src/app/delivery-details/delivery-details.component.ts
+++ src/app/delivery-details/delivery-details.component.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {FormGroup, FormControl} from '@angular/forms';
 import {Router} from '@angular/router';
-import { Project} from '../models/project';
 import * as Wizard from './../store/actions';
 import {Store} from '@ngrx/store';
 
@@ -27,19 +26,27 @@ export class DeliveryDetailsComponent implements OnInit {
   });
 
   backTo() {
-    this.router.navigateByUrl('/project-settings');
+    this.router.navigateByUrl('/wizard/project-settings');
   }
   onSubmit() {
     this.store.dispatch(new Wizard.AddProject(this.deliveryDetailsForm.value));
-    this.router.navigateByUrl('/review');
+    this.router.navigateByUrl('/wizard/review');
   }
 
   constructor(private http: HttpClient, private router: Router, private store: Store<any>) { }
 
   ngOnInit() {
+    this.store.dispatch(new Wizard.ActiveRoute({activeRoute: 3}));
+
     this.store.select('project').subscribe((state => {
-      this.project = state;
-      console.log('state', state);
+      this.deliveryDetailsForm = new FormGroup({
+        addressLine1: new FormControl(state.addressLine1),
+        addressLine2: new FormControl(state.addressLine2),
+        postcode: new FormControl(state.postcode),
+        city: new FormControl(state.city),
+        state: new FormControl(state.state),
+        country: new FormControl(state.country),
+      });
     }));
   }
 
--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,3 +1,16 @@
 export class Project {
   email: string;
+  projectName: string;
+  projectOwner: string;
+  customerName: string;
+  companySite: string;
+  phone: string;
+  language: string;
+  timeZone: string;
+  addressLine1: string;
+  addressLine2: string;
+  postcode: string;
+  city: string;
+  state: string;
+  country: string;
 }
